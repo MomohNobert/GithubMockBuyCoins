c9ee296f905ca8271e6f8b2aa6b0dcd9687b5d2f
MomohNobert <okhai0112@gmail.com>
2020-11-12 22:19:44 +0100

    adjustments to accomodate font

--- src/components/Author/author.component.jsx
+++ src/components/Author/author.component.jsx
@@ -1,19 +1,20 @@
 import './author.styles.css';
 
 const Author = ({ githubData }) => {
-    console.log(githubData)
+    const {avatarUrl, bio, login, name } = githubData;
+    const newAvatarUrl = avatarUrl.replace("s=10", "s=250");
     return (
         <div className="author-container">
             <div className="author-content-container">
                 <div className="author-image-container">
-                    <img src="https://avatars3.githubusercontent.com/u/48828858?s=250&u=203020edbcc2d7cf088215829c5061e57344b3a6&v=4" alt="author" />
+                    <img src={newAvatarUrl} alt="author" />
                 </div>
                 <div className="author-identity-container">
-                    <h3>Momoh Nobert</h3>
-                    <p>MomohNobert</p>
+                    <h3 className="author-identity__name">{name}</h3>
+                    <p  className="author-identity__login">{login}</p>
                 </div>
                 <div className="author-bio-container">
-                    <p>Hacking and slashing through the web</p>
+                    <p  className="author-identity__bio">{bio}</p>
                 </div>
             </div>
         </div>
